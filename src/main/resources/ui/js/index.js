4f24b50d4a23f876d899e4c25bc2471a2738c906
Denis <frenzyx6@gmail.com>
2019-04-25 22:50:04 +0300

    Добавил target в gitignore. Исправил форматирование

--- src/main/resources/ui/js/index.js
+++ src/main/resources/ui/js/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-ReactDOM.render(<h1>Hello</h1>,document.getElementById("app"))
+ReactDOM.render(<h1>Hello</h1>, document.getElementById("app"));

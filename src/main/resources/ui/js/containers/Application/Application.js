7fb9c93ce069d55f9e031aa55c76299c15432076
Denis <frenzyx6@gmail.com>
2019-05-06 23:50:21 +0300

    Добавил Grommet. Использовал компоненты для формы входа и регистрации

--- src/main/resources/ui/js/containers/Application/Application.js
+++ src/main/resources/ui/js/containers/Application/Application.js
@@ -1,25 +1,30 @@
 import React from "react";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "../../store"
 import RegistrationForm from "../RegistrationForm"
-import {BrowserRouter, Route, Link} from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import AuthForm from "../AuthForm";
 import EventFeed from "../EventFeed/EventFeed";
+import { Grommet } from 'grommet';
 
-class Application extends React.Component{
-    render(){
+
+class Application extends React.Component {
+    render() {
         return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Link to={"/"}>Вход</Link>
-                    <Link to={"/registration"}>Регистрация</Link>
-                    
-                    <Route path={"/registration"}  component={RegistrationForm}/>
-                    <AuthForm>
-                        <Route path={"/"} exact render={props=><EventFeed/>}/>
-                    </AuthForm>
-                </BrowserRouter>
-            </Provider>
+            <Grommet plain>
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <Link to={"/"}>Вход</Link>
+                        <Link to={"/registration"}>Регистрация</Link>
+                        <Switch>
+                            <Route path={"/registration"} component={RegistrationForm} />
+                            <AuthForm>
+                                <Route path={"/"} exact render={props => <EventFeed />} />
+                            </AuthForm>
+                        </Switch>
+                    </BrowserRouter>
+                </Provider>
+            </Grommet>
         )
     }
 }
